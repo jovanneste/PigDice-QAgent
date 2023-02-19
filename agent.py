1b7bbea917579c74aac48e6094bffee8adef2619
Joachim Vanneste <2460800V@student.gla.ac.uk>
2023-02-19 09:55:59 +0000

    player 2 added

--- agent.py
+++ agent.py
@@ -3,10 +3,23 @@ import random
 import sys
 
 global qtable
-N=20
+global N
 
-# np.set_printoptions(formatter={'float':"{:6.5g}".format})
+N = 20
 
+def roll():
+    return random.choice([1,2,3,4,5,6])
+
+# scenario 1
+def player2play():
+    score = 0
+    while score<N:
+        n = roll()
+        if n!=0:
+            score+=n
+        else:
+            return 0
+    return 1
 
 
 def step(state, a):
@@ -14,7 +27,7 @@ def step(state, a):
     reward = 0
     if a==0:
         # roll
-        n = random.choice([1,2,3,4,5,6])
+        n = roll()
         if n==1 and state!=0:
             reward = -1
             new_state = 0
@@ -25,25 +38,33 @@ def step(state, a):
     else:
         # hold
         new_state = state
+        # player 2 can play
+        outcome = player2play()
+        if outcome == 1:
+            reward = -1
+            done = True
+        else:
+            reward = 1
 
     if new_state>=N:
         done=True
-        reward=50
-
+        reward=10
+        new_state = N
 
     return new_state, done, reward
 
 
 
 
-qtable = np.zeros((N, 2))
+qtable = np.zeros((N+1, 2))
 
-learning_rate = 0.6
-discount_rate = 0.5
+learning_rate = 0.8
+discount_rate = 0.6
+# explore a lot at the start
 epsilon = 0.9
 decay_rate= 0.005
 
-num_episodes = 1
+num_episodes = 100
 max_steps = 20
 
 print("Training agent...")
@@ -60,23 +81,21 @@ for episode in range(num_episodes):
             # exploit
             action = np.argmax(qtable[state,:])
 
-        print(action)
-
         # perfom action
         new_state, done, reward = step(state, action)
-        if done == True:
-            print("Done in " + str(s) + " iterations")
-            break
-
+       
 
 
         # update q table
         qtable[state, action] += learning_rate * (reward + discount_rate *
-                                            np.max(qtable[new_state,:])-qtable[state,action])
+                                            np.max(qtable[new_state,:])-qtable[state,action]).round(2)
 
 
         state = new_state
-        print("State now = ", state)
+        if done == True:
+            print("Done in " +str(s) + " iterations")
+            break
+
         
     epsilon = np.exp(-decay_rate*episode)
 
